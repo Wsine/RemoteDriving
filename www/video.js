c37656a591210c54634fa347b52d8ad98fe9f6b9
Wsine <weizhy3@hotmail.com>
2016-09-05 16:47:23 +0800

    max the video

--- www/video.js
+++ www/video.js
@@ -7,9 +7,10 @@ $(function() {
 });
 
 function adjustEleHeight() {
-	var img = $("#camera img");
-	img.height(img.width() * 9 / 16);
-	$("#chart").height(img.height());
+    var height = $(window).height() - $("#content").position().top - 20;
+    $("#video").height(height);
+    $("#chart").height(height * 0.5);
+	$("#chart").width($("#chart").height() * 16.0 / 9);
 }
 
 function initChart() {
@@ -25,7 +26,17 @@ function initChart() {
 			show : false
 		},
 		toolbox : {
-			show : false
+			show : true,
+            feature : {
+                myTool1: {
+                    show: true,
+                    title: 'toggle',
+                    icon: 'path://M432.45,595.444c0,2.177-4.661,6.82-11.305,6.82c-6.475,0-11.306-4.567-11.306-6.82s4.852-6.812,11.306-6.812C427.841,588.632,432.452,593.191,432.45,595.444L432.45,595.444z M421.155,589.876c-3.009,0-5.448,2.495-5.448,5.572s2.439,5.572,5.448,5.572c3.01,0,5.449-2.495,5.449-5.572C426.604,592.371,424.165,589.876,421.155,589.876L421.155,589.876z M421.146,591.891c-1.916,0-3.47,1.589-3.47,3.549c0,1.959,1.554,3.548,3.47,3.548s3.469-1.589,3.469-3.548C424.614,593.479,423.062,591.891,421.146,591.891L421.146,591.891zM421.146,591.891',
+                    onclick: function() {
+                        $("#content canvas").toggle();
+                    }
+                }
+            }
 		},
 		series : [{
 			type : 'gauge',
@@ -61,7 +72,8 @@ function initChart() {
             },
             detail : {
                 textStyle: {
-                    fontWeight: 'bolder'
+                    fontWeight: 'bolder',
+                    fontSize: 18
                 }
             },
             data:[{value: 40, name: 'km/h'}]
@@ -74,20 +86,20 @@ function initChart() {
             max:7,
             endAngle:45,
             splitNumber:7,
-            axisLine: {            // 坐标轴线
-                lineStyle: {       // 属性lineStyle控制线条样式
+            axisLine: {
+                lineStyle: {
                     width: 8
                 }
             },
-            axisTick: {            // 坐标轴小标记
-                length:12,        // 属性length控制线长
-                lineStyle: {       // 属性lineStyle控制线条样式
+            axisTick: {
+                length:12,
+                lineStyle: {
                     color: 'auto'
                 }
             },
-            splitLine: {           // 分隔线
-                length:20,         // 属性length控制线长
-                lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+            splitLine: {
+                length:20,
+                lineStyle: {
                     color: 'auto'
                 }
             },
@@ -95,11 +107,15 @@ function initChart() {
                 width:5
             },
             title: {
-                offsetCenter: [0, '-30%'],       // x, y，单位px
+                offsetCenter: [0, '-30%'],
+                textStyle: {
+                    fontSize: 11
+                }
             },
             detail: {
-                textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
-                    fontWeight: 'bolder'
+                textStyle: {
+                    fontWeight: 'bolder',
+                    fontSize: 18
                 }
             },
             data:[{value: 1.5, name: 'x1000\nr/min'}]
